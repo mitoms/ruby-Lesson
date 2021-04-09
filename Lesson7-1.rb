fedd5bebe7abef74891ad75ca7d53ac40355d832
EC2 Default User <ec2-user@ip-172-31-3-23.ec2.internal>
2021-04-09 07:09:28 +0000

    [Finish]2-7.1

---
+++ Lesson7-1.rb
@@ -0,0 +1,18 @@
+puts"キー1入力受付"
+input_key1=gets.to_i
+puts"キー2入力受付"
+input_key2=gets.to_i
+
+puts input_key1*input_key2
+
+
+#回答↓
+#puts "計算をはじめます"
+#puts "2つの値を入力してください"
+#
+#a = gets.to_i
+#b = gets.to_i
+#
+#puts "計算結果を出力します"
+#puts "a*b=#{a * b}"
+#puts "計算を終了します"
